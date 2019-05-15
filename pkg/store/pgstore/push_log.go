53f46180032e8fdb14a9934a32ad76d30012b0d6
Dmitry Kulagin <dkulagin@ozon.ru>
2019-05-15 11:20:01 +0300

    fixed slice error

--- pkg/store/pgstore/push_log.go
+++ pkg/store/pgstore/push_log.go
@@ -9,10 +9,13 @@ import (
 const TextMaxLength = 10 * 1024 * 1024
 
 func (s *PGStore) PushLog(text string) error {
+	if len(text) > TextMaxLength {
+		text = text[:TextMaxLength]
+	}
 	_, err := s.db.Exec(`
 		INSERT INTO "log" (id, text, created)
 		VALUES (NULL, $1, $2)`,
-		text[:TextMaxLength],
+		text,
 		time.Now().UTC(),
 	)
 
