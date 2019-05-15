11c44aa1da62740ef53f7cae2db2ca0c09ce5259
Dmitry Kulagin <dkulagin@ozon.ru>
2019-05-15 11:26:02 +0300

    removed some error from shit command

--- internal/app/lenpenbot/shit.go
+++ internal/app/lenpenbot/shit.go
@@ -1,12 +1,10 @@
 package lenpenbot
 
 import (
-	"errors"
-
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func (i *LenPenBot) Shit(msg *tgbotapi.Message) (*tgbotapi.MessageConfig, error) {
 	// TODO: реализовать
-	return nil, errors.New("some error")
+	return nil, nil
 }
